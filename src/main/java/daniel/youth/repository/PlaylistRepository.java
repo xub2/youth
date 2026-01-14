a9f260b248193d1b7ad81afc99640268703625d2
xuv2 <fish5478@naver.com>
2026-01-14 16:53:35 +0900

    1/14 Playlist 페치 조인 최적화 했다가 인메모리 페이징 시도해서 조조 될뻔 함 -> EntityGraph + BatchSize로 변경

--- src/main/java/daniel/youth/repository/PlaylistRepository.java
+++ src/main/java/daniel/youth/repository/PlaylistRepository.java
@@ -1,14 +1,16 @@
 package daniel.youth.repository;
 
 import daniel.youth.domain.playlist.Playlist;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
-
-    Playlist findTopByOrderByUploadDateDesc();
-
     // 페치 조인 최적화
+    // 생각해보니 이렇게 하면 메모리 페이징 시도
     @Query("select p from Playlist p left join fetch p.items order by p.uploadDate desc limit 1")
     Playlist findTopWithItemsOrderByUploadDateDesc();
+
+    @EntityGraph(attributePaths = {"items"})
+    Playlist findTopByOrderByUploadDateDesc();
 }
